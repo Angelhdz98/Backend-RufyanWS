9cb1d2e4f975187a1323d6d6e0fba1c652f464f9
Angelhdz98 <hernandeztorresjoseangel@gmail.com>
2024-12-12 14:19:13 -0600

    HttpStatus were asigned to the exceptions

--- PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/Exceptions/AlreadyExistIdenticatorException.java
+++ PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/Exceptions/AlreadyExistIdenticatorException.java
@@ -1,5 +1,9 @@
 package com.example.PaginaWebRufyan.Exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.CONFLICT)
 public class AlreadyExistIdenticatorException extends RuntimeException{
  
 	public AlreadyExistIdenticatorException(String message) {
--- PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/Exceptions/InconsitentDataException.java
+++ PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/Exceptions/InconsitentDataException.java
@@ -1,5 +1,9 @@
 package com.example.PaginaWebRufyan.Exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
 public class InconsitentDataException extends RuntimeException{
 
 	public InconsitentDataException(String message) {
