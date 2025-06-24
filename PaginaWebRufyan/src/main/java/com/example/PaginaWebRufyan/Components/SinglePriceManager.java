8082b97770598f521df4ac2178deb71b2ea79a22
Angelhdz98 <hernandeztorresjoseangel@gmail.com>
2025-06-24 14:44:03 -0600

    No arg SinglePriceManager created

--- PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/Components/SinglePriceManager.java
+++ PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/Components/SinglePriceManager.java
@@ -1,14 +1,25 @@
 package com.example.PaginaWebRufyan.Components;
 
 import com.example.PaginaWebRufyan.Entity.Product;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.math.BigDecimal;
 import java.util.Map;
 @Setter
+@Embeddable
+@Getter
+@AllArgsConstructor
+@ToString
 public class SinglePriceManager implements PriceManager {
-
+    private final BigDecimal MIN_PRICE= BigDecimal.valueOf(200);
     private BigDecimal price;
+    public SinglePriceManager(){
+        this.price = MIN_PRICE;
+    }
     @Override
     public Object getPriceData() {
         //Map<String, Object> priceResponse = new HashMap<>();
