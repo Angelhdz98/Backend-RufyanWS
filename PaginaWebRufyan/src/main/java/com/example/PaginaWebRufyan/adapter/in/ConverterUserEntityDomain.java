c63a731e820de381439414f56f94cbf6544497c6
Angelhdz98 <hernandeztorresjoseangel@gmail.com>
2025-08-20 09:36:25 -0600

    Implementación de los métodos para transformar entre UserEntity y userDomain.

--- PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/adapter/in/ConverterUserEntityDomain.java
+++ PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/adapter/in/ConverterUserEntityDomain.java
@@ -1,11 +1,24 @@
 package com.example.PaginaWebRufyan.adapter.in;
 
 import com.example.PaginaWebRufyan.User.Entity.UserEntity;
+import com.example.PaginaWebRufyan.User.Entity.UserProfilePicture;
 import com.example.PaginaWebRufyan.domain.model.UserDomain;
 
 public class ConverterUserEntityDomain {
 
-    UserEntity convertEntityToDomain(UserDomain userDomain){
+   public static UserEntity convertToEntity(UserDomain userDomain, UserProfilePicture profilePicture){
+        return new UserEntity(userDomain.getId(),userDomain.getFullname(),userDomain.getBirthDate(),userDomain.getUsername(),userDomain.getEmail());
+    }
 
+
+    public static UserEntity convertToEntity(UserDomain userDomain){
+        return new UserEntity(userDomain.getId(),userDomain.getFullname(),userDomain.getBirthDate(),userDomain.getUsername(),userDomain.getEmail());
+    }
+    public static UserDomain convertToDomain(UserEntity userEntity, String email,UserProfilePicture profilePicture){
+        return new UserDomain(userEntity.getId(),userEntity.getFullName(), userEntity.getBirthDate(),userEntity.getUsername(),userEntity.getEmail());
+    }
+
+    public static UserDomain convertToDomain(UserEntity userEntity) {
+       return new UserDomain(userEntity.getId(),userEntity.getFullName(), userEntity.getBirthDate(),userEntity.getUsername(),userEntity.getEmail());
     }
 }
