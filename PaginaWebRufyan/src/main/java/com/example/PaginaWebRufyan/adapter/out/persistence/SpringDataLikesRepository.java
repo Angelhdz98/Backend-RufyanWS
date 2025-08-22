1e6130ebe5d20e7b57c988e763ffd2befb35f18b
Angelhdz98 <hernandeztorresjoseangel@gmail.com>
2025-08-22 17:01:37 -0600

    queries added to Count the number of likes in a product

--- PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/adapter/out/persistence/SpringDataLikesRepository.java
+++ PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/adapter/out/persistence/SpringDataLikesRepository.java
@@ -1,17 +1,28 @@
 package com.example.PaginaWebRufyan.adapter.out.persistence;
 
-import com.example.PaginaWebRufyan.domain.model.LikesDomain;
+import com.example.PaginaWebRufyan.domain.model.LikeDomain;
 import com.example.PaginaWebRufyan.domain.model.UserDomain;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Set;
 
-public interface SpringDataLikesRepository extends JpaRepository<LikesDomain, Long> {
-        Set<LikesDomain> findByUserId(Long userId);
-        Set<UserDomain> findByProductId(Long productId);
-        Page<LikesDomain> findByUserId(Long userId, Pageable pageable);
-        Page<LikesDomain> findByProductId(Long productId, Pageable pageable);
+
+public interface SpringDataLikesRepository extends JpaRepository<LikeEntity, Long> {
+        Set<LikeEntity> findByUserId(Long userId);
+        Set<LikeEntity> findByProductId(Long productId);
+        Page<LikeEntity> findByUserId(Long userId, Pageable pageable);
+        Page<LikeEntity> findByProductId(Long productId, Pageable pageable);
+
+        @Query("SELECT COUNT(p1) FROM LikeEntity WHERE p1.productId = :productId")
+        long countLikesByProductId(@Param("productId") Long productId);
+
+        @Query("SELECT COUNT(p1) FROM LikeEntity WHERE p1.userId = :userId")
+        long countLikesByUserId(@Param("userId") Long userId);
+
+
 
 }
