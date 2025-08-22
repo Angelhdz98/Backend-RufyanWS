41ba478ae5e1046fd32b4f38c74040251e99b52a
Angelhdz98 <hernandeztorresjoseangel@gmail.com>
2025-08-22 17:11:26 -0600

    ProductTypeEnum added to ProductSpecsDto so will easy some searches

--- PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/domain/model/ValueObjects/ProductSpecs.java
+++ PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/domain/model/ValueObjects/ProductSpecs.java
@@ -1,9 +1,10 @@
 package com.example.PaginaWebRufyan.domain.model.ValueObjects;
 
 import com.example.PaginaWebRufyan.Image.Image;
+import com.example.PaginaWebRufyan.Products.Enums.ProductTypeEnum;
 
 import java.util.Set;
 
 public record ProductSpecs(String name, Set<Image>productImages, ProductStockDTO productStock,
-                           ProductPricingDTO productPricing) {
+                           ProductPricingDTO productPricing, ProductTypeEnum productTypeEnum) {
 }
