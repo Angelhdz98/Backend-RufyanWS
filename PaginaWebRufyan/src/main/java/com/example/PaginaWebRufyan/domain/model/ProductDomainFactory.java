ec9770fc3903ffb75086752f6d5dba37379e2cf2
Angelhdz98 <hernandeztorresjoseangel@gmail.com>
2025-08-22 17:10:27 -0600

    Factory Pattern implemented to have diferent types of products easily

--- PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/domain/model/ProductDomainFactory.java
+++ PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/domain/model/ProductDomainFactory.java
@@ -1,6 +1,6 @@
 package com.example.PaginaWebRufyan.domain.model;
 
-import com.example.PaginaWebRufyan.Products.Enums.ProductType;
+import com.example.PaginaWebRufyan.Products.Enums.ProductTypeEnum;
 import com.example.PaginaWebRufyan.domain.model.ValueObjects.*;
 import org.springframework.stereotype.Component;
 
@@ -10,7 +10,7 @@ import java.util.Arrays;
 public class ProductDomainFactory {
 
     public ProductDomain createProduct(ProductSpecs productSpecs, ProductDomainDetails productDetails){
-        return switch (productDetails.getProductType()){
+        return switch (productSpecs.productTypeEnum()){
             case PAINTING ->   {
                if(!(productDetails instanceof PaintingDomainDetails paintingDetails)){
                    throw new IllegalArgumentException("No tiene los detalles necesarios:  " + Arrays.toString(PaintingDomainDetails.class.getDeclaredFields()));
@@ -25,12 +25,12 @@ public class ProductDomainFactory {
                 }
 
 
-                yield new PaintingDomain(0,productSpecs.name(),new PaintingStockManager(paintingStock.availableCopies(),paintingStock.copiesMade(),paintingStock.isOriginalAvailable()), new PaintingPriceManager(paintingPricing.pricePerCopy(),paintingPricing.pricePerOriginal()), productSpecs.productImages(), paintingDetails );
+                yield new PaintingDomain(0L,productSpecs.name(),new PaintingStockManager(paintingStock.availableCopies(),paintingStock.copiesMade(),paintingStock.isOriginalAvailable()), new PaintingPriceManager(paintingPricing.pricePerCopy(),paintingPricing.pricePerOriginal()), productSpecs.productImages(), paintingDetails );
             }
             case CUP -> null;
             case CLOTHING -> null;
             case PRINT -> null;
-            default -> throw new IllegalArgumentException("No hay ningun tipo de producto llamado así las opciones son "+ Arrays.toString(ProductType.values()) );
+            default -> throw new IllegalArgumentException("No hay ningun tipo de producto llamado así las opciones son "+ Arrays.toString(ProductTypeEnum.values()) );
         };
 
     }
