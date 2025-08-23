4e18d9fc3b10631dad70572fd2d7bf960d963627
Angelhdz98 <hernandeztorresjoseangel@gmail.com>
2025-08-23 12:50:16 -0600

    new methods implemented for paged searches

--- PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/domain/port/out/JpaLikesRepository.java
+++ PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/domain/port/out/JpaLikesRepository.java
@@ -1,14 +1,20 @@
 package com.example.PaginaWebRufyan.domain.port.out;
 
 import com.example.PaginaWebRufyan.Exceptions.ResourceNotFoundException;
+import com.example.PaginaWebRufyan.adapter.in.LikeControllers.LikeCommand;
+import com.example.PaginaWebRufyan.adapter.out.persistence.LikeEntity;
 import com.example.PaginaWebRufyan.adapter.out.persistence.SpringDataLikesRepository;
-import com.example.PaginaWebRufyan.domain.model.LikesDomain;
-import com.example.PaginaWebRufyan.domain.model.UserDomain;
+import com.example.PaginaWebRufyan.domain.model.LikeDomain;
 import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
+@Repository
 public class JpaLikesRepository implements LikesRepository{
 
     private final SpringDataLikesRepository springDataLikesRepo;
@@ -19,39 +25,51 @@ public class JpaLikesRepository implements LikesRepository{
 
 
     @Override
-    public Set<LikesDomain> findUserLikes(Long userId) {
-       return springDataLikesRepo.findByUserId(userId);
+    public Set<LikeDomain> findUserLikes(Long userId) {
+       return  springDataLikesRepo.findByUserId(userId).stream().map(LikeMapper::mapToDomain).collect(Collectors.toSet());
     }
 
     @Override
-    public LikesDomain markAsLiked(LikesDomain likesDomain) {
-        return springDataLikesRepo.save(likesDomain);
+    public Page<LikeDomain> findUserLikes(Long userId, Pageable pageable) {
+        return springDataLikesRepo.findByUserId(userId,pageable).map(LikeMapper::mapToDomain);
+    }
+
+    @Override
+    public LikeDomain markAsLiked(LikeCommand likeCommand) {
+
+        return LikeMapper.mapToDomain(springDataLikesRepo.save(new LikeEntity(null,likeCommand.userId(),likeCommand.productId()) ));
     }
 
     @Override
     public boolean existsLike(Long userId, Long productId) {
-        return springDataLikesRepo.exists(Example.of(new LikesDomain(userId,productId,null)));
-    }
+        return springDataLikesRepo.exists(Example.of(new LikeEntity(null,userId,productId)));
+     }
 
     @Override
-    public void unmarkAsLiked(Long userId, Long productId) {
-        Optional<LikesDomain> optionalLike= springDataLikesRepo.findOne(Example.of(new LikesDomain(userId,productId,null)));
+    public void unmarkAsLiked(Long userId,Long productId) {
+        Optional<LikeDomain> optionalLike= springDataLikesRepo.findOne(Example.of(new LikeEntity(null, userId,productId))).map(LikeMapper::mapToDomain);
         if(optionalLike.isEmpty()) throw new ResourceNotFoundException("No se ha generado ese like userId: "+ userId+ "productId: "+ productId);
-        springDataLikesRepo.delete(optionalLike.get());
+        springDataLikesRepo.delete(LikeMapper.mapToEntity(optionalLike.get()));
     }
 
     @Override
-    public Set<UserDomain> listUsersLiked(Long productId) {
-        return springDataLikesRepo.findByProductId(productId);
+    public Set<LikeDomain> listUsersLiked(Long productId) {
+        return springDataLikesRepo.findByProductId(productId).stream().map(LikeMapper::mapToDomain).collect(Collectors.toSet());
+    }
+
+    @Override
+    public Page<LikeDomain> listUsersLiked(Long productId, Pageable page) {
+        return springDataLikesRepo.findByProductId(productId,page).map(LikeMapper::mapToDomain);
     }
 
     @Override
     public Long countLikesFromProduct(Long productId) {
-        return 0L;
+        return springDataLikesRepo.countLikesByProductId(productId);
     }
 
     @Override
     public Long countUserLikes(Long userId) {
-        return 0L;
+        return springDataLikesRepo.countLikesByUserId(userId);
     }
+
 }
