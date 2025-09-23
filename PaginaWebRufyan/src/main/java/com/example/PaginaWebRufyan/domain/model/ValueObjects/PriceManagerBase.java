c280a4ec44288d06168712ceb20a5f0ce783df2a
Angelhdz98 <hernandeztorresjoseangel@gmail.com>
2025-09-23 17:03:31 -0600

    Lombok getter added an Id

--- PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/domain/model/ValueObjects/PriceManagerBase.java
+++ PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/domain/model/ValueObjects/PriceManagerBase.java
@@ -1,12 +1,19 @@
 package com.example.PaginaWebRufyan.domain.model.ValueObjects;
 
+import lombok.Getter;
+
 import java.math.BigDecimal;
 import java.util.Map;
 
+@Getter
 public abstract class PriceManagerBase {
-    public final String badge = "MXN";
+    private final Long id;
+    public static final String badge = "MXN";
    public abstract BigDecimal getPriceWithDetails(CartItemDetails details);
    public abstract Map<String,Object> getPriceMap();
 
+    protected PriceManagerBase() {
+         this.id = null;
+    }
 
 }
