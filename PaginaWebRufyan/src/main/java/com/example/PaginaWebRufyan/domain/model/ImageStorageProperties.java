03f016cb7e88f83505ede91a4e145631eb5271f8
Angelhdz98 <hernandeztorresjoseangel@gmail.com>
2025-10-09 09:58:41 -0600

    delete unused imports

--- PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/domain/model/ImageStorageProperties.java
+++ PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/domain/model/ImageStorageProperties.java
@@ -2,10 +2,7 @@ package com.example.PaginaWebRufyan.domain.model;
 
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.stereotype.Component;
 
 @Configuration
 public class ImageStorageProperties {
--- PaginaWebRufyan/src/test/java/com/example/PaginaWebRufyan/domain/model/ImageStoragePropertiesTest.java
+++ PaginaWebRufyan/src/test/java/com/example/PaginaWebRufyan/domain/model/ImageStoragePropertiesTest.java
@@ -1,14 +1,11 @@
 package com.example.PaginaWebRufyan.domain.model;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class ImageStoragePropertiesTest {
 
