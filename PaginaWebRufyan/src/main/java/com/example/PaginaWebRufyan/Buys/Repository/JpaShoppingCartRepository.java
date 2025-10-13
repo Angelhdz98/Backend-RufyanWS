5e7e844453f6787fc786c7e26b45303813dbc612
Angelhdz98 <hernandeztorresjoseangel@gmail.com>
2025-10-13 14:16:43 -0600

    Shopping Cart Entity renamed

--- PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/Buys/Repository/JpaShoppingCartRepository.java
+++ PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/Buys/Repository/JpaShoppingCartRepository.java
@@ -1,13 +1,12 @@
 package com.example.PaginaWebRufyan.Buys.Repository;
 
-import com.example.PaginaWebRufyan.Buys.Entity.ShoppingCart;
-import com.example.PaginaWebRufyan.User.Entity.UserEntity;
+import com.example.PaginaWebRufyan.Buys.Entity.ShoppingCartEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface JpaShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
+public interface JpaShoppingCartRepository extends JpaRepository<ShoppingCartEntity, Long> {
 
-    Optional<ShoppingCart> findCartByUserId(Long userId);
+    Optional<ShoppingCartEntity> findCartByUserId(Long userId);
 
 }
