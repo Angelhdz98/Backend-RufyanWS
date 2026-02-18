2a0e79c9e8b02a02ee20d66021b62387744b70e2
angelhdz98 <hernandeztorresjoseangel@gmail.com>
2026-02-18 13:42:02 -0600

    subtype specification for jackson serialization

--- PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/domain/model/ValueObjects/ProductStockDTO.java
+++ PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/domain/model/ValueObjects/ProductStockDTO.java
@@ -1,4 +1,23 @@
  package com.example.PaginaWebRufyan.domain.model.ValueObjects;
 
+ import com.example.PaginaWebRufyan.adapter.out.ClothingStockManager;
+ import com.example.PaginaWebRufyan.adapter.out.StockManager;
+ import com.fasterxml.jackson.annotation.JsonSubTypes;
+ import com.fasterxml.jackson.annotation.JsonTypeInfo;
+ import lombok.NoArgsConstructor;
+
+
+ @JsonTypeInfo(
+         use = JsonTypeInfo.Id.NAME,
+         include = JsonTypeInfo.As.EXISTING_PROPERTY,
+         property = "stockType",
+         visible = true
+ )
+ @JsonSubTypes({
+         @JsonSubTypes.Type(value = PaintingStockDTO.class, name = "PAINTING"),
+         @JsonSubTypes.Type(value = BodyClothingStockDTO.class, name = "CLOTHING"),
+         @JsonSubTypes.Type(value = SimpleStockDTO.class, name = "SIMPLE")
+ })
 public interface ProductStockDTO {
 }
+
