e84a6aa479d5f6224f6759481f0ebe49f629c22e
angelhdz98 <hernandeztorresjoseangel@gmail.com>
2026-02-18 13:41:00 -0600

    Pricing type enum added for diff and DTO changed to be a class again to control the constructors

--- PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/domain/model/ValueObjects/PaintingPricingDTO.java
+++ PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/domain/model/ValueObjects/PaintingPricingDTO.java
@@ -1,8 +1,26 @@
 package com.example.PaginaWebRufyan.domain.model.ValueObjects;
 
-import java.math.BigDecimal;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-public record PaintingPricingDTO(
-        BigDecimal pricePerOriginal,
-        BigDecimal pricePerCopy) implements ProductPricingDTO {
+import java.math.BigDecimal;
+@NoArgsConstructor
+@Getter
+public class PaintingPricingDTO implements ProductPricingDTO {
+
+    private BigDecimal pricePerOriginal;
+    private BigDecimal pricePerCopy;
+    private PricingTypeEnum pricingType;
+
+    public PaintingPricingDTO(BigDecimal pricePerOriginal, BigDecimal pricePerCopy) {
+        this.pricePerOriginal = pricePerOriginal;
+        this.pricePerCopy = pricePerCopy;
+        this.pricingType = PricingTypeEnum.ORIGINAL;
+    }
+
+    public PaintingPricingDTO(BigDecimal pricePerOriginal, BigDecimal pricePerCopy, PricingTypeEnum pricingType) {
+        this.pricePerOriginal = pricePerOriginal;
+        this.pricePerCopy = pricePerCopy;
+        this.pricingType = PricingTypeEnum.ORIGINAL;
+    }
 }
