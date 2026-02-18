e5f6f34f8dbf8c541beb474655460eb560d0e117
angelhdz98 <hernandeztorresjoseangel@gmail.com>
2026-02-18 13:38:23 -0600

    typo error corrected

--- PaginaWebRufyan/src/test/java/com/example/PaginaWebRufyan/DomainTest/PriceManagerTest/PaintingPriceManagerTest.java
+++ PaginaWebRufyan/src/test/java/com/example/PaginaWebRufyan/DomainTest/PriceManagerTest/PaintingPriceManagerTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -35,8 +36,9 @@ public class PaintingPriceManagerTest {
 
         PaintingPriceManager paintingPriceManager = new PaintingPriceManager(priceGlobalPerCopy,priceGlobalPerOriginal);
 
-assertThat(paintingPriceManager.getPriceWithDetails(originalSelected)).isEqualTo(priceGlobalPerCopy);
+assertThat(paintingPriceManager.getPriceWithDetails(originalSelected)).isEqualTo(priceGlobalPerOriginal);
         assertThat(paintingPriceManager.getPriceWithDetails(copySelected)).isEqualTo(priceGlobalPerCopy.multiply(new BigDecimal(copiesQuantity)));
+
 
     }
 
