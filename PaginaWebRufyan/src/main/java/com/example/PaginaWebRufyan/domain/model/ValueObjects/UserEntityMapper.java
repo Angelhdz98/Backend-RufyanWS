0ae9d1f301f11913932f2778094f428b713bf437
angelhdz98 <hernandeztorresjoseangel@gmail.com>
2026-02-20 15:13:51 -0600

    user entity mapper marked as @Component

--- PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/domain/model/ValueObjects/UserEntityMapper.java
+++ PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/domain/model/ValueObjects/UserEntityMapper.java
@@ -1,11 +1,17 @@
 package com.example.PaginaWebRufyan.domain.model.ValueObjects;
 
+import com.example.PaginaWebRufyan.DTO.CreateUserCommand;
 import com.example.PaginaWebRufyan.DTO.UserEntityDTO2;
 import com.example.PaginaWebRufyan.domain.model.UserDomain;
+import org.springframework.stereotype.Component;
 
+@Component
 public class UserEntityMapper {
 
  public static UserEntityDTO2 toDto(UserDomain userDomain){
     return new UserEntityDTO2(userDomain.getId(),userDomain.getFullname().getFullName(),userDomain.getBirthDate().toString(), userDomain.getEmail(), userDomain.getUsername());
 }
+
+
+
 }
