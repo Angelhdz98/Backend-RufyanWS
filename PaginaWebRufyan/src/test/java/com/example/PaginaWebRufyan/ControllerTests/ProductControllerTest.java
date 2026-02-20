c7f42eaed5aa9cec2a31d15b0ba595bd3a457390
angelhdz98 <hernandeztorresjoseangel@gmail.com>
2026-02-20 15:14:38 -0600

    ProductControllerTest completed

--- PaginaWebRufyan/src/test/java/com/example/PaginaWebRufyan/ControllerTests/ProductControllerTest.java
+++ PaginaWebRufyan/src/test/java/com/example/PaginaWebRufyan/ControllerTests/ProductControllerTest.java
@@ -14,9 +14,12 @@ import com.example.PaginaWebRufyan.domain.model.ProductDomain;
 import com.example.PaginaWebRufyan.domain.model.UserDomain;
 import com.example.PaginaWebRufyan.domain.model.ValueObjects.*;
 import com.example.PaginaWebRufyan.domain.port.in.ProductUseCase.CreateProductCommand;
+import com.example.PaginaWebRufyan.domain.port.in.ProductUseCase.UpdateProductCommand;
 import com.example.PaginaWebRufyan.domain.port.out.UserRepositoryPort;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import io.jsonwebtoken.security.Keys;
 import io.restassured.RestAssured;
+import io.restassured.common.mapper.TypeRef;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +28,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.core.io.ClassPathResource;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -44,6 +49,8 @@ import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @Testcontainers
 @SpringBootTest( classes = PaginaWebRufyanApplication.class,webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT /*, properties = {"spring.servlet.multipart.max-file-size=100MB",
@@ -56,15 +63,16 @@ public class ProductControllerTest {
     ObjectMapper objectMapper;
 
     @Autowired
-    UserRepositoryPort userRepositoryPort;
+    private UserRepositoryPort userRepositoryPort;
 
-    @Value("spring.datasource.admin-password")
-    private String adminPassword;
+    @Value("${spring.datasource.admin-password}")
+     private String adminPassword;
 
 
-    UserDomain admin = new UserDomain(1L, new FullName("Rodrigo", "", "muñoz", "silva"), new BirthDate(LocalDate.of(1997,03,28)),"RufyanSilvaMuralista", "rufyanone@gmail.com", "contra123", RoleEnum.ROLE_ADMIN);
 
-    UserDomain userBuyer = new UserDomain(2L, new FullName("Joel", "Alonso", "Martinez", "Pelaez"), new BirthDate(LocalDate.of(1987,03,28)),"PelaMa", "pelama@gmail.com", "contra2456");
+  static  UserDomain admin = new UserDomain(1L, new FullName("Rodrigo", "", "muñoz", "silva"), new BirthDate(LocalDate.of(1997,03,28)),"RufyanSilvaMuralista", "rufyanone@gmail.com", "contra123", RoleEnum.ROLE_ADMIN);
+
+
 
 
     @Autowired
@@ -73,11 +81,19 @@ public class ProductControllerTest {
     @Autowired
     AuthService authService;
 
+    @Autowired
+    PasswordEncoder encoder = new BCryptPasswordEncoder();
 
+    @Value("${spring.datasource.user-buyer-password}")
+    private String userBuyerPassword;
+    @Value("${spring.datasource.user-buyer-email}")
+    private String userBuyerEmail;
 
     String tokenAdmin;
     String tokenUser;
 
+    static TokenResponse loginUser;
+    static TokenResponse loginAdmin;
 
     Set<File> initialImages= new HashSet<>();
     Set<File> editedImages= new HashSet<>();
@@ -99,6 +115,8 @@ public class ProductControllerTest {
     Integer copiesMade = 10;
     Boolean isOriginalAvailable= true;
 
+    String titulo1= "PRODUCT TEST 1";
+    String titulo2= "product test 2";
     CreateProductCommand product1Command;
     CreateProductCommand product2Command;
     Path p1;
@@ -108,8 +126,6 @@ public class ProductControllerTest {
     byte[] image2Bytes = new byte[50*1024];
     byte[] image3Bytes = new byte[50*1024];
     byte[] image4Bytes = new byte[50*1024];
-
-
 
 
 
@@ -128,29 +144,28 @@ public class ProductControllerTest {
 
     ImageStorageProperties imageStorageProperties;
 
-    public ProductControllerTest() throws IOException {
-    }
 
     @BeforeAll
     static void startDb(){
 
 
 byte[] key = Keys.secretKeyFor(io.jsonwebtoken.SignatureAlgorithm.HS256).getEncoded();
-       // System.out.println("contranesa: "+  Base64.getEncoder().encodeToString(key));
+       // System.out.println("contranesa: "+  Base64.getEncoder().encodeToString(key));;
+
+
+
 
     }
     @BeforeEach
     void setUp(){
 
-        UserDomain foundUser = userRepositoryPort.retrieveUserByEmail(admin.getEmail());
-
-        System.out.println("usuario encontrado: "+ foundUser);
-
-        TokenResponse login = authService.login(new LoginCommand(foundUser.getEmail(), adminPassword));
+        loginUser = authService.login(new LoginCommand(userBuyerEmail, userBuyerPassword));
+        loginAdmin = authService.login(new LoginCommand(admin.getEmail(), adminPassword));
 
 
-        tokenAdmin =  login.accessToken();
-        tokenUser = jwtService.generateToken(userBuyer);
+        tokenAdmin = loginAdmin.accessToken();
+
+        tokenUser = loginUser.accessToken();
 
 
 
@@ -182,8 +197,7 @@ byte[] key = Keys.secretKeyFor(io.jsonwebtoken.SignatureAlgorithm.HS256).getEnco
         //creating commands for new  products
         initialImages = Set.of(file1, file2);
         editedImages = Set.of( file3, file4);
-        String titulo1= "PRODUCT TEST 1";
-        String titulo2= "product test 2";
+
         String description1 = "description product";
         String description2 = "description product2";
          productSpecs1 = new ProductSpecs(titulo1, description1,
@@ -191,6 +205,7 @@ byte[] key = Keys.secretKeyFor(io.jsonwebtoken.SignatureAlgorithm.HS256).getEnco
          productSpecs2 = new ProductSpecs(titulo2, description2,
                 new PaintingStockDTO(true, PaintingStockManager.DEFAULT_STOCK_COPIES+2, PaintingStockManager.DEFAULT_COPIES_MADE+2, StockEnum.PAINTING), new PaintingPricingDTO(PaintingPriceManager.MIN_ORIGINAL_PRICE.add(BigDecimal.valueOf(100)),  PaintingPriceManager.MIN_COPY_PRICE.add(BigDecimal.valueOf(100)), PricingTypeEnum.ORIGINAL ), ProductTypeEnum.PAINTING, false );
         productSpecsUpdated = new ProductSpecs(titulo1.toLowerCase(),description1,  new PaintingStockDTO(true, PaintingStockManager.DEFAULT_STOCK_COPIES, PaintingStockManager.DEFAULT_COPIES_MADE, StockEnum.PAINTING), new PaintingPricingDTO(PaintingPriceManager.MIN_ORIGINAL_PRICE, PaintingPriceManager.MIN_COPY_PRICE, PricingTypeEnum.ORIGINAL), ProductTypeEnum.PAINTING, true );
+
 
 
 
@@ -221,7 +236,6 @@ byte[] key = Keys.secretKeyFor(io.jsonwebtoken.SignatureAlgorithm.HS256).getEnco
         registry.add("spring.datasource.username",mysql::getUsername);
         registry.add("spring.datasource.password",mysql::getPassword);
         registry.add("spring.datasource.driver-class-name", mysql::getDriverClassName);
-        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create");
         registry.add("spring.servlet.multipart.max-file-size", () -> "100MB");
         registry.add("spring.servlet.multipart.max-request-size", () -> "100MB");
     }
@@ -237,15 +251,10 @@ byte[] key = Keys.secretKeyFor(io.jsonwebtoken.SignatureAlgorithm.HS256).getEnco
 
 //    ProductSpecs productSpecs =  new ProductSpecs("Obra de prueba", "Una obra de prueba ");
 
+    public ProductDTO postProduct(String token, Integer statusCode) throws JsonProcessingException {
 
-
-    @Test
-    @DisplayName("Test para probar la creación de un producto correctamente por parte del admin")
-       public void shouldCreateAProduct() throws IOException {
-
-System.out.println(" file 1 size: "+file1.getTotalSpace()+ " file 2 size: "+ file2.getTotalSpace());
-        ProductDTO productDTO = RestAssured.given()
-                .header("Authorization", "Bearer " + tokenAdmin)
+        return RestAssured.given()
+                .header("Authorization", "Bearer " + token)
                 .multiPart("images", "obra1.jpg", image1Bytes, "image/jpeg")
                 .multiPart("images", "obra2.jpg", image2Bytes, "image/jpeg")
                 .multiPart("command", "command.json",
@@ -254,23 +263,65 @@ System.out.println(" file 1 size: "+file1.getTotalSpace()+ " file 2 size: "+ fil
                 .when()
                 .post("/products")
                 .then()
-                .statusCode(200)
+                .statusCode(statusCode)
                 .extract()
                 .as(ProductDTO.class);
 
-    System.out.println(" new productDTO: " +  productDTO);
+
+    }
+
+
+
+    @Test
+    @DisplayName("Test para probar la creación de un producto correctamente por parte del admin")
+       public void shouldCreateAProduct() throws IOException {
+
+//System.out.println(" file 1 size: "+file1.getTotalSpace()+ " file 2 size: "+ file2.getTotalSpace());
+        postProduct(tokenAdmin,200);
+
+//    System.out.println(" new productDTO: " +  productDTO);
     }
 
 
 
     @Test
     @DisplayName("Test para probar la edición de un producto correctamente por parte del admin")
-    public void shouldEditAProduct(){
+    public void shouldEditAProduct() throws JsonProcessingException {
+        ProductDTO productCreated = postProduct(tokenAdmin,200);
+
+        Set<ImageDomain> productImages = productCreated.images();
+
+        UpdateProductCommand updateProductCommand = new UpdateProductCommand(productCreated.id(),productSpecsUpdated, productCreated.productDetails(),  productImages );
+
+        ProductDTO updatedProductDto = RestAssured.given()
+                .header("Authorization", "Bearer " + tokenAdmin)
+                .multiPart("addedImages", "obra3.jpg", image3Bytes, "image/jpeg")
+                .multiPart("updateCommand", "command.json",
+                        objectMapper.writeValueAsBytes(updateProductCommand),
+                        "application/json")
+                .when()
+                .put("/products")
+                .then()
+                .statusCode(200).extract().as(ProductDTO.class);
+
+        assertThat(updatedProductDto.name()).isEqualTo(titulo1.toLowerCase());
+        assertThat(updatedProductDto.images().size()).isEqualTo(3);
+
 
     }
     @Test
     @DisplayName("Test para probar la eliminación de un producto correctamente por parte del admin")
-    public void shouldDeleteAProduct(){
+    public void shouldDeleteAProduct() throws JsonProcessingException {
+        ProductDTO productCreatedDTO = postProduct(tokenAdmin, 200);
+
+         RestAssured.given()
+                .header("Authorization", "Bearer " + tokenAdmin)
+                .when()
+                .delete("/products/" + productCreatedDTO.id().toString())
+                .then()
+                .statusCode(200);
+
+         RestAssured.when().get("/products/" + productCreatedDTO.id()).then().statusCode(404);
 
     }
 
@@ -278,44 +329,92 @@ System.out.println(" file 1 size: "+file1.getTotalSpace()+ " file 2 size: "+ fil
 
     @Test
     @DisplayName("Test para evitar que algun cliente suba un producto")
-    public void shouldThrowExceptionPostingProductWithTokenWithClientRole (){
+    public void shouldThrowExceptionPostingProductWithTokenWithClientRole () throws JsonProcessingException {
 
-    }
+                 RestAssured.given()
+                .header("Authorization", "Bearer " + tokenUser)
+                .multiPart("images", "obra1.jpg", image1Bytes, "image/jpeg")
+                .multiPart("images", "obra2.jpg", image2Bytes, "image/jpeg")
+                .multiPart("command", "command.json",
+                        objectMapper.writeValueAsBytes(product1Command),
+                        "application/json")
+                .when()
+                .post("/products")
+                .then()
+                .statusCode(403);
 
 
-    @Test
-    @DisplayName("Test para evitar que algun cliente elimine un")
-    public void shouldThrowExceptionDeletingProductWithTokenWithClientRole (){
-
-    }
-    @Test
-    @DisplayName("Test para evitar que alguien sin token edite un producto ")
-    public void shouldThrowExceptionEditingProductWithNoToken(){
-
-    }
-    @Test
-    @DisplayName("Test para evitar que alguien sin rol administrador edite un producto  ")
-    public void shouldThrowExceptionEditingProductWithTokenWithClientRole (){
 
     }
 
     @Test
-    @DisplayName("Test para comprobar el usuario en la base de datos evitar JWT artificiales ")
-    public void shouldThrowExceptionPostingEditinOrDeletingProductWithArtificialJWT (){
+    @DisplayName("Test para encontrar una obra usando el controlador ")
+    public void shouldFindAProductById() throws JsonProcessingException {
+        ProductDTO productCreated = postProduct(tokenAdmin,  200);
+
+        ProductDTO foundProduct = RestAssured.when().get("/products/" + productCreated.id())
+                .then()
+                .statusCode(200)
+                .extract().as(ProductDTO.class);
+        assertThat(foundProduct.name()).isEqualTo(productCreated.name());
 
     }
+
+    @Test
+    @DisplayName("Test para evitar que algun cliente elimine un producto")
+    public void shouldThrowExceptionDeletingProductWithTokenWithClientRole () throws JsonProcessingException {
+        ProductDTO productDTO = postProduct(tokenAdmin, 200);
+
+        RestAssured.given()
+                .header("Authorization", "Bearer" + tokenUser)
+                .when()
+                .delete("/products/" + productDTO.id())
+                .then()
+                .statusCode(403);
+
+    }
+    @Test
+    @DisplayName("Test para evitar que alguien sin rol admin edite un producto ")
+    public void shouldThrowExceptionEditingProductWithNoToken() throws JsonProcessingException {
+        ProductDTO productCreated = postProduct(tokenAdmin,200);
+
+        Set<ImageDomain> productImages = productCreated.images();
+
+        UpdateProductCommand updateProductCommand = new UpdateProductCommand(productCreated.id(),productSpecsUpdated, productCreated.productDetails(),  productImages );
+
+RestAssured.given().when()
+        .header("Authorization", "Bearer: "+ tokenUser)
+                .multiPart("addedImages", "obra3.jpg", image3Bytes, "image/jpeg")
+                .multiPart("updateCommand", "command.json",
+                        objectMapper.writeValueAsBytes(updateProductCommand),
+                        "application/json")
+                .when()
+                .put("/products")
+                .then()
+                .statusCode(403);
+
+
+
+    }
+
+
+
     @Test
     @DisplayName("Test para comprobar que la respuesta de una busqueda se un page")
     public void  shouldFindPagedProducts(){
 
-        RestAssured.given()
+        PageResponse<ProductDTO> response = RestAssured.given()
                 .contentType(ContentType.JSON)
                 .when()
                 .get("/products")
                 .then()
                 .statusCode(200)
-                .extract()
-                .as(ProductDTO.class);
+                .extract().as(new TypeRef<PageResponse<ProductDTO>>() {});
+
+        assertThat(response.getContent().size()).isEqualTo(1);
+        assertThat(response.getContent().get(0).name()).isEqualTo(product1Command.productSpecs().name());
+
+
     }
 
 
