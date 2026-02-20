e5671ae994b9001a0a43fc1db8a94187d75eebfe
angelhdz98 <hernandeztorresjoseangel@gmail.com>
2026-02-20 15:05:55 -0600

    password encoded while LogIn

--- PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/adapter/in/UserControllers/LogInController.java
+++ PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/adapter/in/UserControllers/LogInController.java
@@ -5,6 +5,8 @@ import com.example.PaginaWebRufyan.Security.Service.TokenResponse;
 import com.example.PaginaWebRufyan.domain.port.in.userUseCase.LoginUserUseCase;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,13 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @PreAuthorize("permitAll()")
 public class LogInController {
     private final LoginUserUseCase loginUserUseCase;
-
+    private final PasswordEncoder encoder = new BCryptPasswordEncoder();
     public LogInController(LoginUserUseCase loginUserUseCase) {
         this.loginUserUseCase = loginUserUseCase;
     }
 
     @PostMapping("/auth/login")
     public ResponseEntity<TokenResponse> authenticate(@RequestBody LoginCommand loginCommand){
-    return ResponseEntity.ok(loginUserUseCase.authenticate(loginCommand));
+    return ResponseEntity.ok(loginUserUseCase.authenticate(new LoginCommand(loginCommand.identificator(),encoder.encode(loginCommand.password()))));
 }
 }
