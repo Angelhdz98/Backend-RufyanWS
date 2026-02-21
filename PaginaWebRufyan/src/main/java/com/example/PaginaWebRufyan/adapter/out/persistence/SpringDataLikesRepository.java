8aedcf025c2a0a4dc4162ac7fa2275f819253231
angelhdz98 <hernandeztorresjoseangel@gmail.com>
2026-02-21 14:27:48 -0600

    Refactor JpaLikesRepository to use specific find methods for like existence and deletion

--- PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/adapter/out/persistence/SpringDataLikesRepository.java
+++ PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/adapter/out/persistence/SpringDataLikesRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
 import java.util.Set;
 
 
@@ -20,7 +21,11 @@ public interface SpringDataLikesRepository extends JpaRepository<LikeEntity, Lon
 
         @Query("SELECT COUNT(l) FROM Likes l WHERE l.userId = :userId")
         long countLikesByUserId(@Param("userId") Long userId);
+        Optional<LikeEntity> findByUserIdAndProductId(Long userId, Long productId);
 
+        void deleteByUserIdAndProductId(Long userId, Long productId);
+
+        boolean existsByUserIdAndProductId(Long userId, Long productId);
 
 
 }
