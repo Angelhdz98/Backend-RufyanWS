5ae00dffcbc4fe70f888b6f9d04bda5d87d4a2b6
angelhdz98 <hernandeztorresjoseangel@gmail.com>
2026-03-05 12:44:42 -0600

    Class created for create te user client for testing controllers

--- PaginaWebRufyan/src/test/java/com/example/PaginaWebRufyan/ControllerTests/UserDataInitializer.java
+++ PaginaWebRufyan/src/test/java/com/example/PaginaWebRufyan/ControllerTests/UserDataInitializer.java
@@ -1,8 +1,11 @@
 package com.example.PaginaWebRufyan.ControllerTests;
 
+import com.example.PaginaWebRufyan.DTO.CreateUserCommand;
 import com.example.PaginaWebRufyan.domain.model.UserDomain;
 import com.example.PaginaWebRufyan.domain.model.ValueObjects.BirthDate;
+import com.example.PaginaWebRufyan.domain.model.ValueObjects.CreateUserCommandMapper;
 import com.example.PaginaWebRufyan.domain.model.ValueObjects.FullName;
+import com.example.PaginaWebRufyan.domain.port.in.userUseCase.CreateUserUseCase;
 import com.example.PaginaWebRufyan.domain.port.out.UserRepositoryPort;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
@@ -21,18 +24,24 @@ public class UserDataInitializer implements CommandLineRunner {
     @Value("${spring.datasource.user-buyer-email}")
     private String userBuyerEmail;
     private final UserRepositoryPort userRepositoryPort;
+    private final CreateUserUseCase createUserUseCase;
     private final PasswordEncoder passwordEncoder;
+    private final CreateUserCommandMapper createUserCommandMapper;
 
-    public UserDataInitializer(UserRepositoryPort userRepositoryPort) {
+    public UserDataInitializer(UserRepositoryPort userRepositoryPort, CreateUserUseCase createUserUseCase, CreateUserCommandMapper createUserCommandMapper) {
         this.userRepositoryPort = userRepositoryPort;
+        this.createUserUseCase = createUserUseCase;
+        this.createUserCommandMapper = createUserCommandMapper;
         this.passwordEncoder = new BCryptPasswordEncoder();
     }
 
     @Override
     public void run(String... args) throws Exception {
         UserDomain userBuyer = new UserDomain(2L, new FullName("Joel", "Alonso", "Martinez", "Pelaez"), new BirthDate(LocalDate.of(1987,03,28)),"PelaMa", userBuyerEmail, passwordEncoder.encode(userBuyerPassword));
-
-        userRepositoryPort.saveUser(userBuyer);
+        // Creating the user just by repository jumps the shopping cart creation;
+        //userRepositoryPort.saveUser(userBuyer);
+        // Using Service instead;
+        createUserUseCase.createUser(new CreateUserCommand(userBuyerEmail,userBuyerPassword,userBuyer.getUsername(),userBuyer.getFullname(),userBuyer.getBirthDate().getBirthDate()));
 
     }
 }
