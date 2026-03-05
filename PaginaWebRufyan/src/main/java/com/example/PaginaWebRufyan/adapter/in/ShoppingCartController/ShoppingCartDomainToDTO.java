fbe204204d365095f03deebe41e2ce3270b7626b
angelhdz98 <hernandeztorresjoseangel@gmail.com>
2026-03-05 12:34:54 -0600

    Shopping cartDTO changed to be a Component with no static methods

--- PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/adapter/in/ShoppingCartController/ShoppingCartDomainToDTO.java
+++ PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/adapter/in/ShoppingCartController/ShoppingCartDomainToDTO.java
@@ -4,13 +4,15 @@ import com.example.PaginaWebRufyan.Exceptions.ResourceNotFoundException;
 import com.example.PaginaWebRufyan.domain.model.CartItemDomain;
 import com.example.PaginaWebRufyan.domain.model.ShoppingCartDomain;
 import com.example.PaginaWebRufyan.domain.model.ValueObjects.ImageDomain;
+import org.springframework.stereotype.Component;
 
 import java.util.Set;
 import java.util.stream.Collectors;
 
+@Component
 public class ShoppingCartDomainToDTO {
 
-   public static ShoppingCartDTO toDTO(ShoppingCartDomain shoppingCartDomain){
+   public  ShoppingCartDTO toDTO(ShoppingCartDomain shoppingCartDomain){
         Set<CartItemDTORecord> cartItemDTORecordSet = shoppingCartDomain.getItems().stream().map((CartItemDomain item) -> {
             Set<ImageDomain> images = item.getProduct().getImages();
             ImageDomain firstImage = images.stream().findFirst().orElseThrow(() -> new ResourceNotFoundException("El producto no tiene imagenes"));
