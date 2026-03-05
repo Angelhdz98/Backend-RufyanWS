1c99d0836d4595bb7d5963b12fc7d2bacc921067
angelhdz98 <hernandeztorresjoseangel@gmail.com>
2026-03-05 12:21:51 -0600

    Current user Service used to identify the user on controllers

--- PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/adapter/in/LikeControllers/UnmarkAsLikedController.java
+++ PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/adapter/in/LikeControllers/UnmarkAsLikedController.java
@@ -1,9 +1,12 @@
 package com.example.PaginaWebRufyan.adapter.in.LikeControllers;
 
+import com.example.PaginaWebRufyan.Service.UserServiceAdapter.CurrentUserService;
+import com.example.PaginaWebRufyan.domain.model.UserDomain;
 import com.example.PaginaWebRufyan.domain.port.in.LikesUseCase.DislikeUseCase;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,14 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 @PreAuthorize("hasRole('CLIENT')")
 public class UnmarkAsLikedController {
     private final DislikeUseCase dislikeUseCase;
+    private final CurrentUserService currentUserService;
 
-    public UnmarkAsLikedController(DislikeUseCase dislikeUseCase) {
+    public UnmarkAsLikedController(DislikeUseCase dislikeUseCase, CurrentUserService currentUserService) {
         this.dislikeUseCase = dislikeUseCase;
+        this.currentUserService = currentUserService;
     }
 
-    @DeleteMapping("/like")
-    public ResponseEntity<Void> dislike(@RequestBody LikeCommand likeCommand){
-        dislikeUseCase.dislike(likeCommand);
-       return ResponseEntity.noContent().build();
+    @DeleteMapping("/like/{productId}")
+    public ResponseEntity<Void> dislike(@PathVariable Long productId){
+
+        UserDomain currentUser = currentUserService.getCurrentUser();
+        dislikeUseCase.dislike(new LikeCommand(currentUser.getId(), productId));
+        return ResponseEntity.noContent().build();
     }
 }
