74c6f51c5659b9b0af7b5b6e0568e066ae0f0a84
angelhdz98 <hernandeztorresjoseangel@gmail.com>
2026-03-19 12:00:58 -0600

    verificationTokenRepositoryPort userEmailVerificationRepositoryPort and EmailVerificationService used while creating a new user

--- PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/Service/UserServiceAdapter/CreateUserService.java
+++ PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/Service/UserServiceAdapter/CreateUserService.java
@@ -3,11 +3,15 @@ package com.example.PaginaWebRufyan.Service.UserServiceAdapter;
 import com.example.PaginaWebRufyan.DTO.CreateUserCommand;
 import com.example.PaginaWebRufyan.Exceptions.AlreadyExistIdenticatorException;
 import com.example.PaginaWebRufyan.Exceptions.EmailAlreadyUsedException;
+import com.example.PaginaWebRufyan.Service.EmailVerificationServiceAdapter.EmailVerificationService;
+import com.example.PaginaWebRufyan.User.Entity.VerificationToken;
 import com.example.PaginaWebRufyan.domain.model.UserDomain;
 import com.example.PaginaWebRufyan.domain.model.ValueObjects.BirthDate;
 import com.example.PaginaWebRufyan.domain.port.in.userUseCase.CreateUserUseCase;
 import com.example.PaginaWebRufyan.domain.port.out.ShoppingCartRepositoryPort;
+import com.example.PaginaWebRufyan.domain.port.out.UserEmailVerifiedRepositoryPort;
 import com.example.PaginaWebRufyan.domain.port.out.UserRepositoryPort;
+import com.example.PaginaWebRufyan.domain.port.out.VerificationTokenRepositoryPort;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -17,10 +21,16 @@ public class CreateUserService implements CreateUserUseCase {
        private final UserRepositoryPort userRepositoryPort;
        private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
        private final ShoppingCartRepositoryPort shoppingCartRepositoryPort;
+       private final VerificationTokenRepositoryPort verificationTokenRepositoryPort;
+       private final UserEmailVerifiedRepositoryPort userEmailVerifiedRepositoryPort;
+       private final EmailVerificationService emailVerificationService;
 
-    public CreateUserService(UserRepositoryPort userRepositoryPort, ShoppingCartRepositoryPort shoppingCartRepositoryPort) {
+    public CreateUserService(UserRepositoryPort userRepositoryPort, ShoppingCartRepositoryPort shoppingCartRepositoryPort, VerificationTokenRepositoryPort verificationTokenRepositoryPort, UserEmailVerifiedRepositoryPort userEmailVerifiedRepositoryPort, EmailVerificationService emailVerificationService) {
         this.userRepositoryPort = userRepositoryPort;
         this.shoppingCartRepositoryPort = shoppingCartRepositoryPort;
+        this.verificationTokenRepositoryPort = verificationTokenRepositoryPort;
+        this.userEmailVerifiedRepositoryPort = userEmailVerifiedRepositoryPort;
+        this.emailVerificationService = emailVerificationService;
     }
 
 
@@ -32,6 +42,9 @@ public class CreateUserService implements CreateUserUseCase {
         UserDomain newUser = new UserDomain(0L,createUserCommand.getFullName(),new BirthDate(createUserCommand.getBirthDate()),createUserCommand.getUsername(),createUserCommand.getEmail(), passwordEncoder.encode(createUserCommand.getPassword()));
         UserDomain savedUser = userRepositoryPort.saveUser(newUser);
         shoppingCartRepositoryPort.createShoppingCart(savedUser.getId());
+        VerificationToken verificationToken = verificationTokenRepositoryPort.createVerificationToken(savedUser.getId());
+        userEmailVerifiedRepositoryPort.createVerification(savedUser.getId());
+        emailVerificationService.sendVerificationEmail(savedUser.getEmail(),verificationToken.getToken());
         return savedUser;
     }
 }
