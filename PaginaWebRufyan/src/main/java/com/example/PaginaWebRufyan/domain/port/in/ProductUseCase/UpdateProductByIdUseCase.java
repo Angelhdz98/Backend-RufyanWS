62277e280ea3708ac63eb2ab7a71a29a7f0909b0
Angelhdz98 <hernandeztorresjoseangel@gmail.com>
2026-05-13 10:25:34 -0600

    using list instead of set

--- PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/domain/port/in/ProductUseCase/UpdateProductByIdUseCase.java
+++ PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/domain/port/in/ProductUseCase/UpdateProductByIdUseCase.java
@@ -3,8 +3,10 @@ package com.example.PaginaWebRufyan.domain.port.in.ProductUseCase;
 import com.example.PaginaWebRufyan.domain.model.ProductDomain;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Set;
+import java.util.List;
+import java.util.Optional;
+
 
 public interface UpdateProductByIdUseCase {
-    ProductDomain updateProductById(UpdateProductCommand command, Set<MultipartFile> addedImages);
+    ProductDomain updateProductById(UpdateProductCommand command, Optional<List<MultipartFile>> addedImages);
 }
