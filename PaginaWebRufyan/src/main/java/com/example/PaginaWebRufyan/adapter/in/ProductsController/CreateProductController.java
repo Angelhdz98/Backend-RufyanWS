09fb4b2d7265716b7b36c8960aa77b72c1cd6382
Angelhdz98 <hernandeztorresjoseangel@gmail.com>
2026-05-13 10:18:18 -0600

    using list instead of set

--- PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/adapter/in/ProductsController/CreateProductController.java
+++ PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/adapter/in/ProductsController/CreateProductController.java
@@ -12,7 +12,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.HashSet;
+
 import java.util.List;
 
 @RestController
