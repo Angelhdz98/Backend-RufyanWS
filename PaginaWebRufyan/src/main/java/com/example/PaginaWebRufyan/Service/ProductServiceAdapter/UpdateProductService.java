4afda56d099fd6b4e624e985a9caefccfbdba1ec
Angelhdz98 <hernandeztorresjoseangel@gmail.com>
2026-05-13 10:25:15 -0600

    refactor updateProductById to use Optional for addedImages parameter

--- PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/Service/ProductServiceAdapter/UpdateProductService.java
+++ PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/Service/ProductServiceAdapter/UpdateProductService.java
@@ -8,6 +8,9 @@ import com.example.PaginaWebRufyan.domain.port.out.ProductRepositoryPort;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 @Service
@@ -21,8 +24,15 @@ public class UpdateProductService implements UpdateProductByIdUseCase {
     }
 
     @Override
-    public ProductDomain updateProductById(UpdateProductCommand command, Set<MultipartFile> addedImages) {
+    public ProductDomain updateProductById(UpdateProductCommand command, Optional<List<MultipartFile>> addedImages) {
 
-        return productRepositoryPort.updateProduct(productDomainFactory.updateProduct(command, addedImages));
+        if(addedImages.isPresent()){
+            Optional<Set<MultipartFile>> optionalImageSet = addedImages.map(list -> new HashSet<>(list));
+           return productRepositoryPort.updateProduct(productDomainFactory.updateProduct(command, optionalImageSet.get()));
+        }else{
+            return productRepositoryPort.updateProduct(productDomainFactory.updateProduct(command)) ;
+        }
+
+
     }
 }
