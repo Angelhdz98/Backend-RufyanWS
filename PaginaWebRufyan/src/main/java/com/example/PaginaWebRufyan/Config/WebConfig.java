6b0f8734cdcf50d84d9e9ff9e13d8b4d43267f3f
Angelhdz98 <hernandeztorresjoseangel@gmail.com>
2026-05-13 10:26:05 -0600

    exposing a folder to serve images with nginx

--- PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/Config/WebConfig.java
+++ PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/Config/WebConfig.java
@@ -8,13 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-	
-	  @Override
-	    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-	        registry.addResourceHandler("/UploadedImages/**")
-	                .addResourceLocations("file:src/main/resources//static/UploadedImages/")
-	                .setCachePeriod(0); 
-	    }
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/uploads/**")
+				.addResourceLocations("file:/app/uploads/");
+
+	}
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
