add34aadc0adc9239ca1d5d5b22d1b17af4b35cb
Angelhdz98 <hernandeztorresjoseangel@gmail.com>
2026-05-25 13:25:14 -0600

    Implementing use of cookies for saving the refresh token

--- PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/adapter/in/UserControllers/LogInController.java
+++ PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/adapter/in/UserControllers/LogInController.java
@@ -3,10 +3,10 @@ package com.example.PaginaWebRufyan.adapter.in.UserControllers;
 import com.example.PaginaWebRufyan.DTO.LoginCommand;
 import com.example.PaginaWebRufyan.Security.Service.TokenResponse;
 import com.example.PaginaWebRufyan.domain.port.in.userUseCase.LoginUserUseCase;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,13 +15,23 @@ import org.springframework.web.bind.annotation.RestController;
 @PreAuthorize("permitAll()")
 public class LogInController {
     private final LoginUserUseCase loginUserUseCase;
-    private final PasswordEncoder encoder = new BCryptPasswordEncoder();
     public LogInController(LoginUserUseCase loginUserUseCase) {
         this.loginUserUseCase = loginUserUseCase;
     }
 
     @PostMapping("/auth/login")
-    public ResponseEntity<TokenResponse> authenticate(@RequestBody LoginCommand loginCommand){
-    return ResponseEntity.ok(loginUserUseCase.authenticate(new LoginCommand(loginCommand.identificator(),encoder.encode(loginCommand.password()))));
+    public ResponseEntity<TokenResponse> authenticate(@RequestBody LoginCommand loginCommand, HttpServletResponse response){
+        System.out.println("La contraseña ingresada es:"+loginCommand.password() + "El email ingresado es: "+ loginCommand.identificator());
+        TokenResponse authResponse =
+                loginUserUseCase.authenticate(loginCommand);
+        Cookie cookie = new Cookie("refreshToken",
+                authResponse.refreshToken());
+        cookie.setHttpOnly(true);
+        cookie.setSecure(false);
+        cookie.setMaxAge(7 * 24 * 60 * 60); // 7 days
+        cookie.setPath("/");
+        cookie.setAttribute("SameSite", "Lax");
+        response.addCookie(cookie);
+        return ResponseEntity.ok(authResponse);
 }
 }
