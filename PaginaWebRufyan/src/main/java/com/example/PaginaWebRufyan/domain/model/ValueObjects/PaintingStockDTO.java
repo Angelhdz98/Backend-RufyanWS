f72e2988f65fcb2d8fd5dc5ce84408300aa0c2e4
Angelhdz98 <hernandeztorresjoseangel@gmail.com>
2026-05-25 13:25:57 -0600

    changing parameter name to match with Front

--- PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/domain/model/ValueObjects/PaintingStockDTO.java
+++ PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/domain/model/ValueObjects/PaintingStockDTO.java
@@ -10,27 +10,27 @@ import lombok.ToString;
 public class PaintingStockDTO implements ProductStockDTO {
 
    private final Boolean isOriginalAvailable;
-   private final Integer availableCopies;
+   private final Integer stockCopies;
    private final Integer copiesMade;
    private final StockEnum stockType;
 
-    public PaintingStockDTO(Boolean isOriginalAvailable, Integer availableCopies, Integer copiesMade) {
+    public PaintingStockDTO(Boolean isOriginalAvailable, Integer stockCopies, Integer copiesMade) {
         this.isOriginalAvailable = isOriginalAvailable;
-        this.availableCopies = availableCopies;
+        this.stockCopies = stockCopies;
         this.copiesMade = copiesMade;
         this.stockType = StockEnum.PAINTING_STOCK;
     }
 
-    public PaintingStockDTO(Boolean isOriginalAvailable, Integer availableCopies, Integer copiesMade, StockEnum stockType) {
+    public PaintingStockDTO(Boolean isOriginalAvailable, Integer stockCopies, Integer copiesMade, StockEnum stockType) {
         this.isOriginalAvailable = isOriginalAvailable;
-        this.availableCopies = availableCopies;
+        this.stockCopies = stockCopies;
         this.copiesMade = copiesMade;
         this.stockType = StockEnum.PAINTING_STOCK;
     }
 
     public PaintingStockDTO(){
         this.isOriginalAvailable= PaintingStockManager.DEFAULT_ORIGINAL_AVAILABLE;
-        this.availableCopies = PaintingStockManager.DEFAULT_STOCK_COPIES;
+        this.stockCopies = PaintingStockManager.DEFAULT_STOCK_COPIES;
         this.copiesMade = PaintingStockManager.DEFAULT_COPIES_MADE;
         this.stockType = StockEnum.PAINTING_STOCK;
     }
