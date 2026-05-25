adc8785b2a12e43f3680fb67cef81ea9f4ab7561
Angelhdz98 <hernandeztorresjoseangel@gmail.com>
2026-05-25 13:18:21 -0600

    the implementation of refreshToken is not working well

--- PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/adapter/in/UserControllers/RefreshTokenController.java
+++ PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/adapter/in/UserControllers/RefreshTokenController.java
@@ -1,23 +1,57 @@
 package com.example.PaginaWebRufyan.adapter.in.UserControllers;
 
+
+import com.example.PaginaWebRufyan.Exceptions.InvalidTokenException;
+import com.example.PaginaWebRufyan.Security.Service.JwtService;
 import com.example.PaginaWebRufyan.Security.Service.TokenResponse;
+import com.example.PaginaWebRufyan.Service.UserServiceAdapter.CurrentUserService;
+import com.example.PaginaWebRufyan.domain.model.UserDomain;
 import com.example.PaginaWebRufyan.domain.port.in.userUseCase.RefreshTokenUseCase;
-import org.springframework.http.HttpHeaders;
+import com.example.PaginaWebRufyan.domain.port.out.UserRepositoryPort;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 @RestController
 public class RefreshTokenController {
     private final RefreshTokenUseCase refreshTokenUseCase;
+    private final JwtService jwtService;
+    private final UserRepositoryPort userRepositoryPort;
 
-
-    public RefreshTokenController(RefreshTokenUseCase refreshTokenUseCase) {
+    public RefreshTokenController(RefreshTokenUseCase refreshTokenUseCase, JwtService jwtService, CurrentUserService currentUserService, UserRepositoryPort userRepositoryPort) {
         this.refreshTokenUseCase = refreshTokenUseCase;
+        this.jwtService = jwtService;
+        this.userRepositoryPort = userRepositoryPort;
     }
     @PostMapping("/auth/refresh")
-    public ResponseEntity<TokenResponse> refreshAuth (@RequestHeader(HttpHeaders.AUTHORIZATION) final String authHeader){
-       return ResponseEntity.ok(refreshTokenUseCase.refreshToken(authHeader));
+    public ResponseEntity<TokenResponse> refreshAuth(HttpServletRequest request) {
+        try {
+            Cookie[] cookies = request.getCookies();
+            System.out.println("Cookies:"+ cookies);
+            if (request.getCookies() != null) {
+                for (Cookie c : request.getCookies()) {
+                    System.out.println(c.getName() + "=" + c.getValue());
+                }
+            }
+            String refreshToken = Arrays.stream(cookies)
+                    .filter(cookie -> cookie.getName().equals("refreshToken"))
+                    .findFirst()
+                    .map(Cookie::getValue)
+                    .orElseThrow(() -> new RuntimeException("No refresh token"));
+            String userEmail =
+                    jwtService.extractEmail(refreshToken);
+            UserDomain currentUser = userRepositoryPort.retrieveUserByEmail(userEmail);
+            if (!jwtService.isTokenValid(refreshToken, currentUser)) {
+                throw new InvalidTokenException("Invalid refresh token");
+            }
+            String newAccessToken = jwtService.generateToken(currentUser);
+            return ResponseEntity.ok(refreshTokenUseCase.refreshToken(newAccessToken));
+        } catch (io.jsonwebtoken.ExpiredJwtException e) {
+            return ResponseEntity.status(401).build();
+        }
     }
 }
