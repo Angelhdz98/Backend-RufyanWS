86192f4c66857374d9aca5b1397af25d88a4cd67
Angelhdz98 <hernandeztorresjoseangel@gmail.com>
2026-05-26 20:30:18 -0600

    permitAll() in findFavoriteProduct while developing

--- PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/adapter/in/ProductsController/FindFavoriteProductsController.java
+++ PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/adapter/in/ProductsController/FindFavoriteProductsController.java
@@ -3,9 +3,11 @@ package com.example.PaginaWebRufyan.adapter.in.ProductsController;
 import com.example.PaginaWebRufyan.adapter.in.ProductDTO;
 import com.example.PaginaWebRufyan.domain.port.in.ProductUseCase.FindFavoriteProductsUseCase;
 import com.example.PaginaWebRufyan.domain.port.out.ProductDTOMapper;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@PreAuthorize("permitAll()")
 public class FindFavoriteProductsController {
     private final FindFavoriteProductsUseCase findFavoriteProductsUseCase;
 
