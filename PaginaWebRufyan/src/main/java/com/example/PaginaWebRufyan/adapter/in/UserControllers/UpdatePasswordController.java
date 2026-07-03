8af18375280bd83d6f1daa0b7521495bfbd78abf
Angelhdz98 <hernandeztorresjoseangel@gmail.com>
2026-07-03 14:43:29 -0600

    Add UpdatePasswordCommand and refactor UpdatePasswordController to use request body

--- PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/adapter/in/UserControllers/UpdatePasswordController.java
+++ PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/adapter/in/UserControllers/UpdatePasswordController.java
@@ -1,15 +1,15 @@
 package com.example.PaginaWebRufyan.adapter.in.UserControllers;
 
 import com.example.PaginaWebRufyan.Service.UserServiceAdapter.CurrentUserService;
+import com.example.PaginaWebRufyan.User.DTO.UpdatePasswordCommand;
 import com.example.PaginaWebRufyan.domain.model.UserDomain;
 import com.example.PaginaWebRufyan.domain.port.in.userUseCase.UpdatePasswordUseCase;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@PreAuthorize()
 public class UpdatePasswordController {
     private final UpdatePasswordUseCase updateUserPasswordUseCase;
     private final CurrentUserService currentUserService;
@@ -19,12 +19,13 @@ public class UpdatePasswordController {
         this.currentUserService = currentUserService;
     }
 
-    @PutMapping("/users/update-password")
-    public ResponseEntity<Void> updatePassword(@RequestPart String oldPassword, @RequestPart String newPassword, @RequestPart String newPasswordConfirmation){
+    @PutMapping("/user/update-password")
+    public ResponseEntity<Void> updatePassword(@RequestBody UpdatePasswordCommand updatePasswordCommand){
         UserDomain currentUser = currentUserService.getCurrentUser();
-        if(!newPassword.equals(newPasswordConfirmation))throw new IllegalArgumentException("La confirmación de contraseña no coincide");
-
-        updateUserPasswordUseCase.updatePassword(currentUser.getId(),oldPassword,newPassword);
+        if( !updatePasswordCommand.newPassword().equals(updatePasswordCommand.newPasswordConfirmation())) {
+            throw new IllegalArgumentException("La confirmación de contraseña no coincide");
+        }
+        updateUserPasswordUseCase.updatePassword(currentUser.getId(),updatePasswordCommand.oldPassword(),updatePasswordCommand.newPassword());
 
         return ResponseEntity.ok().build();
 
