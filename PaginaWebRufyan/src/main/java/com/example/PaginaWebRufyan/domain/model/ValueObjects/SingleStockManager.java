2210f8a1cacc66adb759e1a6e14ef137caef4c79
Angelhdz98 <hernandeztorresjoseangel@gmail.com>
2026-08-14 16:44:58 -0600

    renaming a variable to avoid confusion with other stock

--- PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/domain/model/ValueObjects/SingleStockManager.java
+++ PaginaWebRufyan/src/main/java/com/example/PaginaWebRufyan/domain/model/ValueObjects/SingleStockManager.java
@@ -1,37 +1,38 @@
 package com.example.PaginaWebRufyan.domain.model.ValueObjects;
 
-import com.example.PaginaWebRufyan.domain.model.CartItemDomain;
 import com.example.PaginaWebRufyan.domain.model.ProductDomain;
+import lombok.Getter;
 
 import java.util.Map;
 
 public class SingleStockManager extends StockManagerBase {
-    private Integer stock;
+    @Getter
+    private Integer stockQuantity;
 
     public SingleStockManager(Integer stock) {
-        this.stock = stock;
+        this.stockQuantity = stock;
     }
 
     @Override
     public Map<String, Integer> getStock() {
-        return Map.of("stock",stock);
+        return Map.of("stock", stockQuantity);
     }
 
     @Override
     public void decreaseStock(ProductDomain productDomain, CartItemDetails details) {
         Integer itemQuantity = details.getItemQuantity().getQuantity();
-        if(itemQuantity>this.stock) throw new IllegalArgumentException("El stock actual es de: " + stock+ " petición de: " + itemQuantity + " no puede ser procesada");
-        this.stock = stock-itemQuantity;
+        if(itemQuantity>this.stockQuantity) throw new IllegalArgumentException("El stock actual es de: " + stockQuantity + " petición de: " + itemQuantity + " no puede ser procesada");
+        this.stockQuantity = stockQuantity -itemQuantity;
     }
 
     @Override
     public void increaseStock(ProductDomain productDomain, CartItemDetails details) {
         Integer itemQuantity = details.getItemQuantity().getQuantity();
-        this.stock = stock+itemQuantity;
+        this.stockQuantity = stockQuantity +itemQuantity;
     }
 
     @Override
     public Boolean isAvailable() {
-        return stock>0;
+        return stockQuantity >0;
     }
 }
